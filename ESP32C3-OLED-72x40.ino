4f7d2441d06ff62ec38a8da029d2eb63fb257ad5
Alex Yeryomin <106690273+AlexYeryomin@users.noreply.github.com>
2025-01-01 19:08:49 -0500

    A demo written in C to be used with Arduino IDE.

---
+++ ESP32C3-OLED-72x40.ino
@@ -0,0 +1,29 @@
+#include <Wire.h>
+#include <U8g2lib.h>
+
+// Pins: clock=6, data=5.
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE, 6, 5);
+
+// SSD1306 controller has 132x64 pixel buffer
+const unsigned int BufferWidth  = 132;
+const unsigned int BufferHeight = 64;
+const unsigned int ScreenWidth  = 72;
+const unsigned int ScreenHeight = 40;
+const unsigned int xOffset = (BufferWidth - ScreenWidth) / 2;
+const unsigned int yOffset = (BufferHeight - ScreenHeight) / 2;
+ 
+void setup(void) {
+  delay(1000);
+  u8g2.begin();
+  u8g2.setContrast(255);
+  u8g2.setBusClock(400000);
+  u8g2.setFont(u8g2_font_ncenB10_tr);
+  u8g2.drawFrame(xOffset + 0, yOffset + 0, ScreenWidth, ScreenHeight);
+  u8g2.setCursor(xOffset + (ScreenWidth/2 - 20), yOffset + ScreenHeight/2);
+  u8g2.printf("Hello");
+  u8g2.sendBuffer();
+}
+ 
+void loop(void) {
+}
+ 
